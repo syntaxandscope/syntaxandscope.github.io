a8665bfebac6341466fb4df79b7f3bd82e4753f5
Mamo YZ <yzmamo@gmail.com>
2020-02-11 21:55:51 +0300

    homepage desktop ready

--- js/main.js
+++ js/main.js
@@ -15,32 +15,23 @@ $(function() {
   var pageMainTitle = document.getElementById("pageMainTitle");
 
   var typewriter = new Typewriter(pageMainTitle, {
-    loop: false
+    loop: true
   });
 
   typewriter
     .pauseFor(5500)
-    .typeString("< / >")
-    .pauseFor(500)
-    .deleteChars(3)
-    .typeString("<strong>We Bring</strong>")
+  
+    .typeString("We Bring")
     .pauseFor(300)
     .typeString("<strong> experience,</strong>")
-    .pauseFor(300)
-    .typeString("<br><strong>compliance and</strong>")
-    .typeString(" / ")
-    .pauseFor(500)
-    .typeString(">")
-    .pauseFor(300)
-
-    .deleteChars(3)
-    .pauseFor(300)
-    .typeString("<strong> standards</strong>")
-    .pauseFor(300)
-    .typeString("<br><strong>professional services</strong>")
-    .pauseFor(500)
-    .typeString(" / ")
-    .pauseFor(500)
-    .typeString(">")
+    .pauseFor(1000)
+    .deleteChars(11)
+    .typeString("<strong>compliance </strong>")
+    .pauseFor(1000)
+    .deleteChars(11)
+    .typeString("<strong>and standards</strong>")
+    .pauseFor(1000)
+    .typeString("<br>to professional services")
+    .pauseFor(1000)
     .start();
 });
