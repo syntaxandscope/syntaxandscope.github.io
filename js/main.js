0f1844e047503bc1c0f394a6067bf1b400881157
Mamo YZ <yzmamo@gmail.com>
2020-02-11 15:27:11 +0300

    animation enhance

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
 $(function() {
+  var pageMainTitle = document.getElementById("pageMainTitle");
 
+  var typewriter = new Typewriter(pageMainTitle, {
+    loop: false
+  });
+
+  typewriter
+    .pauseFor(5000)
+    .typeString("<")
+    .pauseFor(300)
+    .typeString(" / ")
+    .pauseFor(300)
+    .typeString(">")
+    .pauseFor(500)
+    .deleteChars(3)
+    .typeString("<strong>We Bring</strong>")
+    .pauseFor(300)
+    .typeString("<strong> experience,</strong>")
+    .pauseFor(300)
+    .typeString("<br><strong>compliance and</strong>")
+    .typeString(" / ")
+    .pauseFor(500)
+    .typeString(">")
+    .pauseFor(300)
+
+    .deleteChars(3)
+    .pauseFor(300)
+    .typeString("<strong> standards</strong>")
+    .pauseFor(300)
+    .typeString("<br><strong>professional services</strong>")
+    .pauseFor(500)
+    .typeString(" / ")
+    .pauseFor(500)
+    .typeString(">")
+    .start();
 });
